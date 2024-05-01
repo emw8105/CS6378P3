ab8c663e15a9faec8371951514a41b87548a9e1d
PJ Avinash <avinashindian2.0@gmail.com>
2024-05-01 18:10:02 -0500

    scripts

--- Launcher/freeports.py
+++ Launcher/freeports.py
@@ -2,23 +2,20 @@ import argparse
 import subprocess
 import re
 import socket
-from multiprocessing import Process
-import os
-
-currentHost = socket.gethostname()
-current_directory = os.getcwd()
 
 # Define argument parser to accept config file
 parser = argparse.ArgumentParser()
 parser.add_argument("userid", help = "name of user(netid)")
 parser.add_argument("config", help="Path to configuration file")
-
 args = parser.parse_args()
+
+# Read the netID and current hostname
+currentHost = socket.gethostname()
 
 # Open and read the config file
 with open(args.config, "r") as f:
     lines = f.readlines()
-    
+
 hosts = {}
 # Loop through the lines and extract the hostname and port number using regex
 for line in lines:
@@ -29,13 +26,11 @@ for line in lines:
         hosts[hostname] = port
 
 # Loop through each host and SSH into it to execute command
-remotehosts = list(hosts.keys())
-for host in remotehosts:
+for host, port in hosts.items():
     if currentHost == host:
-        # need to cd to the server directory to run the server programs
-        command = f"cd .. && java CS6378P3.Client.Main Launcher/{args.config}"
+        ssh_command = f"fuser -k {port}/tcp"
     else:
-        command = ( "ssh -f " + args.userid + "@" + host + " 'cd " + current_directory[:-len("Launcher")]+ " && java CS6378P3.Client.Main Launcher/"+ args.config+ "'")
-    print(command)
-    process = subprocess.Popen(command, shell=True)
-    process.wait()
+        ssh_command = f"ssh {args.config}@{host} 'fuser -k {port}/tcp'"
+    print(ssh_command)
+    process = subprocess.Popen(ssh_command, shell=True)
+    process.wait()  # Wait for the process to finish
